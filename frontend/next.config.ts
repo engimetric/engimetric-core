5670f8d65e4182483733fbd28315404baccd24e7
Ryan Spoone <ryanspoone@gmail.com>
2024-12-27 13:55:30 +0000

    Add NEXT_PUBLIC_API_BASE_URL to NextConfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    },
     distDir: 'build', // Change the build directory from `.next` to `build`
 };
 
