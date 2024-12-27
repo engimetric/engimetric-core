1344b72d4546399360dd814313df0ccd7760a132
Ryan Spoone <ryanspoone@gmail.com>
2024-12-27 13:11:22 +0000

    Make some tweaks to next.js builds; add .env.production template for backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    distDir: 'build', // Change the build directory from `.next` to `build`
 };
 
 export default nextConfig;
